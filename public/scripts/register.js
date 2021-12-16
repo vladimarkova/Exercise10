e5347bfa0ef6671be4c06713044c0e9387d31e8c
Vladi Markova <vladislavam1996@gmail.com>
2021-12-16 14:00:46 +0200

    nice user register with feedback

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,10 +1,33 @@
 (() => {
-    const registerBtn = document.querySelector('#register-btn');
-    registerBtn.addEventListener('click', (e) => {
+    const URL = 'http://localhost:3000';
+
+    // const registerBtn = document.querySelector('#register-btn');
+    const registerForm = document.querySelector('#register-form');
+    const responseElement = document.querySelector('#response');
+    registerForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        fetch('http://localhost:3000/user')
+        const username = document.querySelector('#reg-username');
+        const psw = document.querySelector('#reg-psw');
+        const user = {
+            username: username.value,
+            psw: psw.value
+        };
+
+        fetch('http://localhost:3000/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
             .then(res => res.json())
-            .then(users => console.log(users))
+            .then(data => {
+                console.log(data.message);
+                username.value = '';
+                psw.value = '';
+                responseElement.innerHTML = data.message;
+                responseElement.setAttribute('class', 'show-response');
+            })
             .catch(err => console.error(err))
     })
 })();
--- server.js
+++ server.js
@@ -30,6 +30,22 @@ app.get('/user', (req, res) => {
     res.json(sanitizedUsers);
 })
 
+app.post('/user', (req, res) => {
+    const { username, psw } = req.body;
+    if (!username || !psw) {
+        return res.status(404).json({ message: 'Username and password are required!'});
+    }
+    const userExists = data.users.find(user => user.username === username);
+    if (userExists) {
+        return res.status(409).json({ message: `User with username ${username} already exists!` });
+    }
+    const newUser = { username, psw };
+    data.setUsers([...data.users, newUser]);
+    // console.log(data.users);
+
+    res.status(201).json({ message: `User ${username} created.`});
+})
+
 // app.use('/api', auth, apiRouter);
 
 app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}...`));
