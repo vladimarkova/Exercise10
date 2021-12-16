1e6b09ab7ab5b3b600fc3e2de67af8c054c42481
Vladi Markova <vladislavam1996@gmail.com>
2021-12-16 21:25:58 +0200

    clean the server, add rout index and user router instead

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router;
-const apiRouter = Router();
-const userRouter = require('./userRouter');
-
-apiRouter.use('/user', userRouter);
-
-module.exports = apiRouter;
--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -1,3 +1,20 @@
 const Router = require('express').Router;
 const userRouter = Router();
 
+const data = {
+    users: require('../../data/users.json'),
+    setUsers: function(newUsers) {
+        this.users = newUsers;
+    }
+};
+
+userRouter.route('/')
+    .get((req, res) => {
+        const sanitizedUsers = data.users.map((user) => {
+            return {
+                username: user.username
+            }})
+            res.json(sanitizedUsers);
+    })
+
+module.exports = userRouter;
