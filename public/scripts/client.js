47c74537f2479064f51c0ab943c27ee641a60440
Vladi Markova <vladislavam1996@gmail.com>
2021-12-16 20:40:23 +0200

    some pages fixes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,35 @@
 (() => {
+    const registerLink = document.getElementById('register-link');
+    const loginLink = document.getElementById('login-link');
+    const homeLink = document.getElementById('home-link');
+    const aboutLink = document.getElementById('about-link');
+
     const root = document.getElementById('root');
     const registerFormTemplate = document.getElementById('register-form-template');
     const loginFormTemplate = document.getElementById('login-form-template');
+    const aboutTemplate = document.getElementById('about-template');
+    const test = document.getElementById('test');
 
-    console.log(root.firstElementChild);
+    registerLink.addEventListener('click', () => {
+        root.firstElementChild?.remove();
+        const { content } = registerFormTemplate.cloneNode(true);
+        root.appendChild(content);
+        // console.log(root.firstElementChild);
+    })
 
-    root.firstElementChild?.remove();
-    const { content } = registerFormTemplate.cloneNode(true);
-    root.appendChild(content);
+    loginLink.addEventListener('click', () => {
+        // console.log(registerFormTemplate);
+        // root.firstElementChild?.remove();
+        root.firstElementChild?.remove();
+        const { content } = loginFormTemplate.cloneNode(true);
+        root.appendChild(content);
+        // console.log(root.firstElementChild);
+    })
+
+    aboutLink.addEventListener('click', () => {
+        root.firstElementChild?.remove();
+        const { content } = aboutTemplate.cloneNode(true);
+        root.appendChild(content);
+    })
+
 })();
