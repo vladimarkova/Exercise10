a129ef6b05d582c2af1b60ca7d1b02152ed6cd7b
Vladi Markova <vladislavam1996@gmail.com>
2021-12-16 16:24:32 +0200

    nice beginning of a single page application

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+(() => {
+    const root = document.getElementById('root');
+    const registerFormTemplate = document.getElementById('register-form-template');
+    const loginFormTemplate = document.getElementById('login-form-template');
+
+    console.log(root.firstElementChild);
+
+    root.firstElementChild?.remove();
+    const { content } = registerFormTemplate.cloneNode(true);
+    root.appendChild(content);
+})();
